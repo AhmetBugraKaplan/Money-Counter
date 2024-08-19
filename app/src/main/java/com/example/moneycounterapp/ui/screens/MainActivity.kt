c5e73eba3796f51b17c94dfee1109c941dd480ee
AhmetBugraKaplan <bugrakaplann5@gmail.com>
2024-08-19 15:43:09 +0300

    Money Counter Project

---
+++ app/src/main/java/com/example/moneycounterapp/ui/screens/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.moneycounterapp.ui.screens
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.moneycounterapp.ui.components.MoneyCounterPage
+import com.example.moneycounterapp.ui.theme.MoneyCounterAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MoneyCounterPage()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MoneyCounterAppTheme {
+        Greeting("Android")
+    }
+}
